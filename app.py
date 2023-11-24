376e2c413cef82e04881da178dd1aa9fccdd88f9
shibosenn <2366541442@qq.com>
2023-11-24 13:28:25 +0800

    add 3.5

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import Flask, render_template, request, jsonify
 import requests
+import openai
 
 def new2_chat2gpt4(prompt):
     url = "https://openkey.cloud/v1/chat/completions"
@@ -18,6 +19,23 @@ def new2_chat2gpt4(prompt):
     response = requests.post(url, headers=headers, json=data)  # ['choices'][0]['message']['content']
     return response.json()['choices'][0]['message']['content']
 
+def chat2gpt35_2(messages):
+
+    openai.api_type = "azure"
+    openai.api_base = "https://dptech.openai.azure.com/"
+    openai.api_version = "2023-03-15-preview"
+    openai.api_key = "2a2fbcf82115400e87d94056fecd91bd"
+    gpt_name = "gpt-35"
+    messages_dict = {"role": "user", "content": messages}
+
+    answer = openai.ChatCompletion.create(
+        engine=gpt_name,
+        messages=[messages_dict],
+        temperature=0.8,
+        top_p=0.95
+    )['choices'][0]['message']['content']
+    return answer
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -27,8 +45,10 @@ def home():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.get_json()['message']  # 获取用户消息
-    gpt4_response = new2_chat2gpt4(user_message)  # 将用户消息传给new2_chat2gpt4函数
-    return gpt4_response  # 返回GPT-4的响应
+    # gpt4_response = new2_chat2gpt4(user_message)  # 将用户消息传给new2_chat2gpt4函数
+    gpt35_response = chat2gpt35_2('message')
+    # return gpt4_response  # 返回GPT-4的响应
+    return gpt35_response
 
 if __name__ == "__main__":
     app.run(port=5000)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import openai
 
 def new2_chat2gpt4(prompt):
     url = "https://openkey.cloud/v1/chat/completions"
@@ -8,12 +10,34 @@ def new2_chat2gpt4(prompt):
     }
 
     data = {
-        "model": "gpt-4-0613",
+        "model": "gpt-3",
         "messages": [{"role": "user", "content": prompt}],
 
     }
     response = requests.post(url, headers=headers, json=data)  # ['choices'][0]['message']['content']
-    return response.json()['choices'][0]['message']['content']
+    # return response.json()['choices'][0]['message']['content']
+    return response
+
+
+
+
+def chat2gpt35_2(messages):
+    openai.api_type = "azure"
+    openai.api_base = "https://dptech.openai.azure.com/"
+    openai.api_version = "2023-03-15-preview"
+    openai.api_key = "2a2fbcf82115400e87d94056fecd91bd"
+    gpt_name = "gpt-35"
+
+    messages_dict = {"role": "user", "content": messages}
+
+    answer = openai.ChatCompletion.create(
+        engine=gpt_name,
+        messages=[messages_dict],
+        temperature=0.8,
+        top_p=0.95
+    )['choices'][0]['message']['content']
+    return answer
 
 if __name__ == "__main__":
-    print(new2_chat2gpt4("hello, can you hear me"))
+    # print(new2_chat2gpt4("hello, can you hear me"))
+    print(chat2gpt35_2("nihao"))
